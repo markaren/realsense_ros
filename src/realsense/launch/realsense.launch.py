85b7c4588357aa9e9d2ab7330e5205d04f8df0a2
Lars Ivar Hatledal <laht@ntnu.no>
2026-01-16 11:57:20 +0100

    restructure

--- src/realsense/launch/realsense.launch.py
+++ src/realsense/launch/realsense.launch.py
@@ -7,12 +7,9 @@ from ament_index_python import get_package_share_directory
 
 
 def generate_launch_description():
-    pkg = 'realsense'
-    pkg_share = get_package_share_directory(pkg)
-
 
     camera_node = Node(
-        package=pkg,
+        package='realsense',
         executable='realsense',
         name='realsense',
         output='screen',
@@ -20,7 +17,7 @@ def generate_launch_description():
     )
 
     image_vis_node = Node(
-        package=pkg,
+        package='realsense_utils',
         executable='image_visualizer',
         name='image_visualizer',
         output='screen',
@@ -28,7 +25,7 @@ def generate_launch_description():
     )
 
     points_vis_node = Node(
-        package=pkg,
+        package='realsense_utils',
         executable='points_visualizer',
         name='points_visualizer',
         output='screen',
@@ -48,7 +45,7 @@ def generate_launch_description():
         name='rviz2',
         output='screen',
         parameters=[{"use_sim_time": False}],
-        arguments=['-d', os.path.join(pkg_share, 'config', 'config.rviz')]
+        arguments=['-d', os.path.join(get_package_share_directory('realsense'), 'config', 'config.rviz')]
     )
 
     return LaunchDescription([
